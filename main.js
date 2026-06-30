97d878ed35b84ac726cfebf345269f9b098e35d7
Erika Coreth <erika.coreth@wawa.com>
2026-06-30 11:26:58 -0400

    greeting, temperature, and time added

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 
+
 document.querySelector('#open-nav-menu').addEventListener('click', function() {
     document.querySelector('header nav .wrapper').classList.add('nav-open');
 });
 
+
 document.querySelector('#close-nav-menu').addEventListener('click', function() {
     document.querySelector('header nav .wrapper').classList.remove('nav-open');
 });
+
+// Greeting Section
+
+function celsiusToFahr(temperature) {
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
+const greetingText = "Good Afternoon!";
+const weatherCondition = "sunny";
+const userLocation = "Phily";
+let temperature = 30;
+
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
+
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("#weather").innerHTML = celsiusText;
+
+document.querySelector(".weather-group").addEventListener("click", function(e){
+
+    if (e.target.id === "celsius") {
+        document.querySelector("#weather").innerHTML = celsiusText;
+
+    } else if (e.target.id === "fahr") {
+        document.querySelector("#weather").innerHTML = fahrText;
+    }
+
+});
+
+
+setInterval(function(){
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
+},1000);
