527572131fae8e7b60d136e2fadd2c6bf9d4298e
Erika Coreth <erika.coreth@wawa.com>
2026-06-30 13:41:55 -0400

    Gallery

--- main.js
+++ main.js
@@ -41,7 +41,51 @@ document.querySelector(".weather-group").addEventListener("click", function(e){
 
 setInterval(function(){
     let localTime = new Date();
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
 },1000);
+// Gallery Section
+// src="/assets/gallery/image1.jpg" alt="image 1" />
+
+const galleryImages = [
+    {
+        src: "/assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "/assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    }, 
+    {
+        src: "/assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    }
+];
+
+/* for (let i in galleryImages){
+    console.log(galleryImages[i]);
+} */
+
+
+let mainImage = document.querySelector("#gallery > img");
+let thumbnails = document.querySelector("#gallery .thumbnails");
+
+
+
+mainImage.src = galleryImages[0].src;
+mainImage.alt = galleryImages[0].alt;
+
+// <img src="./assets/gallery/image1.jpg" 
+// alt="Thumbnail Image 1" data-array-index="0" 
+// data-selected="true"></img>
+
+galleryImages.forEach(function(image, index){
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.dataset.arrayIndex = index;
+    thumb.dataset.selected = false;
+    thumbnails.appendChild(thumb);
+
+});
