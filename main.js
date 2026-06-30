0c5f77ebcf4362d8816ec0e44ca04b03969bf185
Erika Coreth <erika.coreth@wawa.com>
2026-06-30 14:29:05 -0400

    code layout fix

--- main.js
+++ main.js
@@ -1,33 +1,55 @@
 
+// Menu Section
 
-document.querySelector('#open-nav-menu').addEventListener('click', function() {
+const galleryImages = [
+    {
+        src: "/assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "/assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    }, 
+    {
+        src: "/assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    }
+];
+
+function menuHandler() {
+    document.querySelector('#open-nav-menu').addEventListener('click', function() {
     document.querySelector('header nav .wrapper').classList.add('nav-open');
 });
 
 
-document.querySelector('#close-nav-menu').addEventListener('click', function() {
+    document.querySelector('#close-nav-menu').addEventListener('click', function() {
     document.querySelector('header nav .wrapper').classList.remove('nav-open');
 });
+    
 
-// Greeting Section
+}
+
+
 
 function celsiusToFahr(temperature) {
     let fahr = (temperature * 9/5) + 32;
     return fahr;
 }
 
-let currentHour = new Date().getHours();
-let greetingText;
+// Greeting Section
+function greetingHandler() {
+    let currentHour = new Date().getHours();
+    let greetingText;
 
-if (currentHour < 12) {
-    greetingText = "Good Morning!";
-} else if (currentHour < 19) {
-    greetingText = "Good Afternoon!";
-} else if (currentHour < 24) {
-    greetingText = "Good Evening!";
-} else {
-    greetingText = "Welcome!";
-}
+    if (currentHour < 12) {
+        greetingText = "Good Morning!";
+    } else if (currentHour < 19) {
+        greetingText = "Good Afternoon!";
+    } else if (currentHour < 24) {
+        greetingText = "Good Evening!";
+    } else {
+        greetingText = "Welcome!";
+    }
 
 
 const weatherCondition = "sunny";
@@ -51,70 +73,67 @@ document.querySelector(".weather-group").addEventListener("click", function(e){
 
 });
 
+}
 
-setInterval(function(){
+// Local Time Section
+
+function clockHandler() {
+    setInterval(function(){
     let localTime = new Date();
     document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
     document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
     document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
 },1000);
+
+}
+
+
+
 // Gallery Section
-// src="/assets/gallery/image1.jpg" alt="image 1" />
 
-const galleryImages = [
-    {
-        src: "/assets/gallery/image1.jpg",
-        alt: "Thumbnail Image 1"
-    },
-    {
-        src: "/assets/gallery/image2.jpg",
-        alt: "Thumbnail Image 2"
-    }, 
-    {
-        src: "/assets/gallery/image3.jpg",
-        alt: "Thumbnail Image 3"
-    }
-];
-
-/* for (let i in galleryImages){
-    console.log(galleryImages[i]);
-} */
-
-
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery .thumbnails");
+function galleryHandler() {
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
 
 
 
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
 
-// <img src="./assets/gallery/image1.jpg" 
-// alt="Thumbnail Image 1" data-array-index="0" 
-// data-selected="true"></img>
 
-galleryImages.forEach(function(image, index){
-    let thumb = document.createElement("img");
-    thumb.src = image.src;
-    thumb.alt = image.alt;
-    thumb.dataset.arrayIndex = index;
-    thumb.dataset.selected = index === 0 ? true : false;
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.selected = index === 0 ? true : false;
 
-    thumb.addEventListener("click", function(e){
-        let selectedIndex = e.target.dataset.arrayIndex;
-        let selectedImage = galleryImages[selectedIndex];
-        mainImage.src = selectedImage.src;
-        mainImage.alt = selectedImage.alt;
+        thumb.addEventListener("click", function(e){
+            let selectedIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectedIndex];
+            mainImage.src = selectedImage.src;
+            mainImage.alt = selectedImage.alt;
 
-        thumbnails.querySelectorAll("img").forEach(function(img){
-            img.dataset.selected = false;
+            thumbnails.querySelectorAll("img").forEach(function(img){
+                img.dataset.selected = false;
+            });
+
+            e.target.dataset.selected = true;
+
         });
 
-        e.target.dataset.selected = true;
+
+        thumbnails.appendChild(thumb);
 
     });
+    
+
+}
 
 
-    thumbnails.appendChild(thumb);
+// Page Load
 
-});
+menuHandler();
+greetingHandler();
+clockHandler();
+galleryHandler();
