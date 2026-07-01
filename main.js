bacc09abde6eccd9cd311d4768c08458abaef9ed
Erika Coreth <erika.coreth@wawa.com>
2026-07-01 09:16:23 -0400

    product filter completed

--- main.js
+++ main.js
@@ -16,6 +16,46 @@ const galleryImages = [
     }
 ];
 
+const products = [
+    {
+      title: "AstroFiction",
+      author: "John Doe",
+      price: 49.9,
+      image: "./assets/products/img6.png"
+    },
+    {
+      title: "Space Odissey",
+      author: "Marie Anne",
+      price: 35,
+      image: "./assets/products/img1.png"
+    },
+    {
+      title: "Doomed City",
+      author: "Jason Cobert",
+      price: 0,
+      image: "./assets/products/img2.png"
+    },
+    {
+      title: "Black Dog",
+      author: "John Doe",
+      price: 85.35,
+      image: "./assets/products/img3.png"
+    },
+    {
+      title: "My Little Robot",
+      author: "Pedro Paulo",
+      price: 0,
+      image: "./assets/products/img5.png"
+    },
+    {
+      title: "Garden Girl",
+      author: "Ankit Patel",
+      price: 45,
+      image: "./assets/products/img4.png"
+    }
+  ];
+
+
 function menuHandler() {
     document.querySelector('#open-nav-menu').addEventListener('click', function() {
     document.querySelector('header nav .wrapper').classList.add('nav-open');
@@ -28,7 +68,6 @@ function menuHandler() {
     
 
 }
-
 
 
 function celsiusToFahr(temperature) {
@@ -87,8 +126,6 @@ function clockHandler() {
 
 }
 
-
-
 // Gallery Section
 
 function galleryHandler() {
@@ -130,6 +167,154 @@ function galleryHandler() {
 
 }
 
+// Products Section
+
+function populateProducts(productList) {
+
+
+    let productsSection = document.querySelector(".products-area");
+    productsSection.textContent = "";
+
+
+    //     run a loop through the products and create a HTML element  ("product-item") for each of them.
+    productList.forEach(function(product, index) {
+        // create the HTML element for the individual product
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+
+        //Create the product image
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+
+
+        // Create the product details section
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        // Create the product title, author, price-title and price
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add("product-title");
+        productTitle.textContent = product.title;
+
+        let productAuthor = document.createElement("p");
+        productAuthor.classList.add("product-author");
+        productAuthor.textContent = product.author;
+
+        let priceTitle = document.createElement("p");
+        priceTitle.classList.add("price-title");
+        priceTitle.textContent = "Price";
+
+        let productPrice = document.createElement("p");
+        productPrice.classList.add("product-price");
+        productPrice.textContent = product.price > 0 ? "$" + product.price.toFixed(2) : "Free";
+
+        
+
+        // Append the product details
+        productDetails.append(productTitle);
+        productDetails.append(productAuthor);
+        productDetails.append(priceTitle);
+        productDetails.append(productPrice);
+
+
+        // Add all child  HTML elements of the product
+        productElm.append(productImage);
+        productElm.append(productDetails);  
+
+
+        // add complete individual project to the product section
+        productsSection.append(productElm);
+    })
+
+}
+
+function productsHandler() {
+
+    let productsSection = document.querySelector(".products-area");
+    let freeProducts = products.filter(function(item){
+        return !item.price || item.price <= 0;
+    });
+    let paidProducts = products.filter(function(item){
+        return item.price > 0;
+    });
+
+//     run a loop through the products and create a HTML element  ("product-item") for each of them.
+    products.forEach(function(product, index) {
+        // create the HTML element for the individual product
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+
+        //Create the product image
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+
+
+        // Create the product details section
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        // Create the product title, author, price-title and price
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add("product-title");
+        productTitle.textContent = product.title;
+
+        let productAuthor = document.createElement("p");
+        productAuthor.classList.add("product-author");
+        productAuthor.textContent = product.author;
+
+        let priceTitle = document.createElement("p");
+        priceTitle.classList.add("price-title");
+        priceTitle.textContent = "Price";
+
+        let productPrice = document.createElement("p");
+        productPrice.classList.add("product-price");
+        productPrice.textContent = product.price > 0 ? "$" + product.price.toFixed(2) : "Free";
+
+        
+
+        // Append the product details
+        productDetails.append(productTitle);
+        productDetails.append(productAuthor);
+        productDetails.append(priceTitle);
+        productDetails.append(productPrice);
+
+
+        // Add all child  HTML elements of the product
+        productElm.append(productImage);
+        productElm.append(productDetails);  
+
+
+        // add complete individual project to the product section
+        productsSection.append(productElm);
+    })
+
+    populateProducts(products);
+
+    document.querySelector(".products-filter label[for=all] span.product-amount").textContent = products.length;
+    document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = paidProducts.length;
+    document.querySelector(".products-filter label[for=free] span.product-amount").textContent = freeProducts.length;
+
+    let productFilter = document.querySelector(".products-filter");
+    productFilter.addEventListener("click", function(e){
+        if (e.target.id === "all") {
+            populateProducts(products);
+        } else if (e.target.id === "paid") {
+            populateProducts(paidProducts);
+        } else if (e.target.id === "free") {
+            populateProducts(freeProducts);
+        }
+    })
+}
+
+function footerHandler() {
+    let currentYear = new Date().getFullYear();
+    document.querySelector("footer").textContent = `© ${currentYear} - All right reserved`;
+
+}
 
 // Page Load
 
@@ -137,3 +322,5 @@ menuHandler();
 greetingHandler();
 clockHandler();
 galleryHandler();
+productsHandler();
+footerHandler();
